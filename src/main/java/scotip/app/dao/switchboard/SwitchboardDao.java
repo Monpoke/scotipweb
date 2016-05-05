1bcb5d8614c4fce95aaee361434abb4261ed1dce
Aurélien SVEVI <sveviaurelien@gmail.com>
2016-05-05 18:39:52 +0200

    ajout de l'annuaire des switchboard

--- src/main/java/scotip/app/dao/switchboard/SwitchboardDao.java
+++ src/main/java/scotip/app/dao/switchboard/SwitchboardDao.java
@@ -1,6 +1,7 @@
 package scotip.app.dao.switchboard;
 
 import scotip.app.model.Switchboard;
+import java.util.List;
 
 /**
  * Created by Pierre on 29/04/2016.
@@ -11,4 +12,6 @@ public interface SwitchboardDao {
     Switchboard get(int sid);
 
     Switchboard getWithModules(int sid);
+
+    List<Switchboard> getAllSwitchboard();
 }
--- src/main/java/scotip/app/service/switchboard/SwitchboardService.java
+++ src/main/java/scotip/app/service/switchboard/SwitchboardService.java
@@ -4,6 +4,8 @@ import scotip.app.dto.SwitchboardDto;
 import scotip.app.model.Company;
 import scotip.app.model.Switchboard;
 
+import java.util.List;
+
 /**
  * Created by Pierre on 29/04/2016.
  */
@@ -13,4 +15,6 @@ public interface SwitchboardService {
     Switchboard getSwitchboardWithIdAndCompany(int sid, Company company);
 
     void notifyServerDialplanReload(Switchboard switchboard);
+
+    List<Switchboard> getAllSwitchboard();
 }
